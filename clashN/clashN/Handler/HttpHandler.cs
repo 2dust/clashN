6b9557a48855746348cc3673eb19b70165efe9b3
jx <jx@jvxiang.com>
2022-11-01 21:57:16 +0800

    添加pac.txt

--- clashN/clashN/Handler/HttpHandler.cs
+++ clashN/clashN/Handler/HttpHandler.cs
@@ -3,6 +3,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Windows;
 using clashN.Mode;
+using clashN.Properties;
 using static System.Net.WebRequestMethods;
 using File = System.IO.File;
 
@@ -16,11 +17,10 @@ public class HttpHandler
 
     public static void Start(Config config)
     {
-        var path = Path.Combine(Utils.StartupPath(), "pac.txt");
+        var path = Path.Combine(Utils.GetConfigPath(), "pac.txt");
         if (!File.Exists(path))
         {
-            MessageBox.Show($"pac file is not exists", "error", MessageBoxButton.OK, MessageBoxImage.Error);
-            return;
+            File.AppendAllText(path, Resources.ResourceManager.GetString("pac"));
         }
 
         _pacText = File.ReadAllText(path).Replace("__PROXY__", $"PROXY 127.0.0.1:{config.httpPort};DIRECT;");
@@ -32,21 +32,29 @@ public class HttpHandler
         {
             while (_isRunning)
             {
-                var client = _tcpListener.AcceptTcpClient();
-                Task.Factory.StartNew(() =>
+                try
                 {
-                    var stream = client.GetStream();
-                    var sb = new StringBuilder();
-                    sb.AppendLine("HTTP/1.0 200 OK");
-                    sb.AppendLine("Content-type:text/plain;charset=UTF-8");
-                    sb.AppendLine("Connection:close");
-                    sb.AppendLine("Content-Length:" + Encoding.UTF8.GetByteCount(_pacText));
-                    sb.AppendLine();
-                    sb.Append(_pacText);
-                    var content = Encoding.UTF8.GetBytes(sb.ToString());
-                    stream.Write(content, 0, content.Length);
-                    stream.Flush();
-                });
+                    var client = _tcpListener.AcceptTcpClient();
+                    Task.Factory.StartNew(() =>
+                    {
+                        var stream = client.GetStream();
+                        var sb = new StringBuilder();
+                        sb.AppendLine("HTTP/1.0 200 OK");
+                        sb.AppendLine("Content-type:text/plain;charset=UTF-8");
+                        sb.AppendLine("Connection:close");
+                        sb.AppendLine("Content-Length:" + Encoding.UTF8.GetByteCount(_pacText));
+                        sb.AppendLine();
+                        sb.Append(_pacText);
+                        var content = Encoding.UTF8.GetBytes(sb.ToString());
+                        stream.Write(content, 0, content.Length);
+                        stream.Flush();
+                    });
+                }
+                catch (Exception e)
+                {
+                    
+                }
+                
             }
             
         });
