9bdf9e8ada08581af283342970a2f2e25efe7d5a
2dust <31833384+2dust@users.noreply.github.com>
2023-01-29 15:56:21 +0800

    Optimize font

---
+++ clashN/clashN/Converters/MaterialDesignFonts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media;
+
+namespace clashN.Converters
+{
+    public class MaterialDesignFonts
+    {
+        public static FontFamily MyFont { get; }
+
+        static MaterialDesignFonts()
+        {
+            var fontPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Resources\Fonts\");
+            MyFont = new FontFamily(new Uri($"file:///{fontPath}"), "./#MiSans");
+        }
+    }
+}
